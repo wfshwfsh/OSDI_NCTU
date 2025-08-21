198b0470b7437f36324cc3f48889b01a9fab41ab
Will.Chen <Will.Chen@advantech.com>
2025-08-22 01:09:03 +0800

    update to rpi3 (arm32 => aarch64)

--- labs/qemu_launch.sh
+++ labs/qemu_launch.sh
@@ -1,4 +1,3 @@
-qemu-system-arm -M raspi2 -m 1G -kernel kernel.img -serial stdio -display none
+qemu-system-aarch64 -M raspi3 -m 1G -kernel kernel.img -serial stdio -display none
 
-
-#qemu-system-arm -M raspi2 -m 256M -kernel kernel.img -S -s
+#qemu-system-aarch64 -M raspi3 -m 1G -kernel kernel.img -S -s
