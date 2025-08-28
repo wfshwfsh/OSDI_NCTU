06814484003110b27fb64ceea4b472ec7a44180d
Will8.Chen <Will8.Chen@advantech.com.tw>
2025-08-28 17:18:52 +0800

    update lab1: timestamp, reboot, and mini-uart

--- labs/01/bootloader.c
+++ labs/01/bootloader.c
@@ -1,24 +1,14 @@
 #include <stdint.h>
+#include "io.h"
 #include "util.h"
 
-#define MMIO_BASE       0x3F000000
-/* Auxilary mini UART registers */
-#define AUX_ENABLE ((volatile unsigned int*)(MMIO_BASE + 0x00215004))
-#define AUX_MU_IO ((volatile unsigned int*)(MMIO_BASE + 0x00215040))
-#define AUX_MU_IER ((volatile unsigned int*)(MMIO_BASE + 0x00215044))
-#define AUX_MU_IIR ((volatile unsigned int*)(MMIO_BASE + 0x00215048))
-#define AUX_MU_LCR ((volatile unsigned int*)(MMIO_BASE + 0x0021504C))
-#define AUX_MU_MCR ((volatile unsigned int*)(MMIO_BASE + 0x00215050))
-#define AUX_MU_LSR ((volatile unsigned int*)(MMIO_BASE + 0x00215054))
-#define AUX_MU_MSR ((volatile unsigned int*)(MMIO_BASE + 0x00215058))
-#define AUX_MU_SCRATCH ((volatile unsigned int*)(MMIO_BASE + 0x0021505C))
-#define AUX_MU_CNTL ((volatile unsigned int*)(MMIO_BASE + 0x00215060))
-#define AUX_MU_STAT ((volatile unsigned int*)(MMIO_BASE + 0x00215064))
-#define AUX_MU_BAUD ((volatile unsigned int*)(MMIO_BASE + 0x00215068))
 
-#define UART0_BASE 0x3F201000
-#define UART0_DR   ((volatile unsigned int*)(UART0_BASE + 0x00))
-#define UART0_FR   ((volatile unsigned int*)(UART0_BASE + 0x18))
+#if 0
+/* Uart0: PL011-UART */
+void uart1_init() {
+
+    
+}
 
 void uart_send(char c) {
     while (*UART0_FR & (1 << 5)); // TXFF bit, transmit FIFO full
@@ -31,22 +21,98 @@ void uart_puts(const char* str) {
         uart_send(*str++);
     }
 }
+#else
+/* Uart1: mini-UART */
+void uart1_init() {
 
-char uart_getc() {
+    register unsigned int r;
+    
+    // 1. Set AUXENB register to enable mini UART. Then mini UART register can be accessed.
+    *AUX_ENABLE = (1<<0);
+    
+    // 2. Set AUX_MU_CNTL_REG to 0. Disable transmitter and receiver during configuration.
+    *AUX_MU_CNTL = 0;
+    
+    // 3. Set AUX_MU_IER_REG to 0. Disable interrupt because currently you don’t need interrupt.
+    *AUX_MU_IER = 0;
+    
+    // 4. Set AUX_MU_LCR_REG to 3. Set the data size to 8 bit.
+    *AUX_MU_LCR = 3;
+    
+    // 5. Set AUX_MU_MCR_REG to 0. Don’t need auto flow control.
+    *AUX_MU_MCR = 0;
+    
+    // 6. Set AUX_MU_BAUD to 270. Set baud rate to 115200: 270 = (250MHz/8/115200)-1
+    *AUX_MU_BAUD = 270;
+    
+    // 7. Set AUX_MU_IIR_REG to 6. (bit[1]=1 Clear Rx FIFO, bit[2]=1 Clear Tx FIFO)
+    *AUX_MU_IIR = 6;
+
+    /* map UART1 to GPIO pins */
+    //r = *GPFSEL1;
+    //r &= ~((7 << 12) | (7 << 15));  // gpio14, gpio15
+    //r |= (2 << 12) | (2 << 15);     // alt5
+    //*GPFSEL1 = r;
+    //*GPPUD = 0;  // enable pins 14 and 15
+    //r = 150;
+    //while (r--) {
+    //    asm volatile("nop");
+    //}
+    //*GPPUDCLK0 = (1 << 14) | (1 << 15);
+    //r = 150;
+    //while (r--) {
+    //    asm volatile("nop");
+    //}
+    //*GPPUDCLK0 = 0;    // flush GPIO setup
+
+
+    // 8. Set AUX_MU_CNTL_REG to 3. Enable the transmitter and receiver
+    *AUX_MU_CNTL |= 3;
+    
+}
+
+void uart1_send(char c) {
+    
+    do{
+        asm volatile("nop");
+    }while ( !(*AUX_MU_LSR & (1<<5)) );
+    *AUX_MU_IO = c;
+}
+
+void uart1_puts(const char* str) {
+    while (*str) {
+        if (*str == '\n') uart1_send('\r'); // 加上 CRLF
+        uart1_send(*str++);
+    }
+}
+
+char uart1_getc() {
     char r;
     /* wait until something is in the buffer */
     do {
         asm volatile("nop");
-    } while (!(*AUX_MU_LSR & 0x01));
+    } while (!(*AUX_MU_LSR & (1<<0)));
     /* read it and return */
     r = (char)(*AUX_MU_IO);
     /* convert carrige return to newline */
     return r == '\r' ? '\n' : r;
 }
+#endif
 
-void print_s(char *ch) { uart_puts(ch); }
-void print_c(char ch) { uart_send(ch); }
-char read_c() { return uart_getc(); }
+char read_c() { return uart1_getc(); }
+void print_s(char *ch) { uart1_puts(ch); }
+void print_c(char ch) { uart1_send(ch); }
+void print_i(int x) {
+    if (x < 0) {
+        print_c('-');
+        x = -x;
+    }
+    if (x >= 10) print_i(x / 10);
+    print_c(x % 10 + '0');
+}
+
+
+
 
 void read_cmd(char *cmd) {
     char now;
@@ -71,6 +137,25 @@ void read_cmd(char *cmd) {
     cmd[now_cur] = 0;
 }
 
+#define PM_PASSWORD 0x5a000000
+#define PM_RSTC ((volatile unsigned int*)0x3F10001c)
+#define PM_WDOG ((volatile unsigned int*)0x3F100024)
+
+void reset(int tick){ // reboot after watchdog timer expire
+  //set(PM_RSTC, PM_PASSWORD | 0x20); // full reset
+  //set(PM_WDOG, PM_PASSWORD | tick); // number of watchdog tick
+  
+  *PM_RSTC = (PM_PASSWORD | 0x20);
+  *PM_WDOG = (PM_PASSWORD | tick);
+}
+
+void cancel_reset() {
+  //set(PM_RSTC, PM_PASSWORD | 0); // full reset
+  //set(PM_WDOG, PM_PASSWORD | 0); // number of watchdog tick
+  
+  *PM_RSTC = (PM_PASSWORD | 0);
+  *PM_WDOG = (PM_PASSWORD | 0);
+}
 
 void shell()
 {
@@ -85,11 +170,18 @@ void shell()
             "timestamp : print system timestamp\n"
             "reboot    : reboot the device\r\n");
 	}else if(0 == strcmp(cmd, "hello")){
-		;
+		print_s("Hello World!\n");
 	}else if(0 == strcmp(cmd, "timestamp")){
-		;
+        uint64_t tm_cnt, tm_frq, tm_sec, tm_co;
+        asm volatile("mrs %0, cntpct_el0" : "=r"(tm_cnt));
+        asm volatile("mrs %0, cntfrq_el0" : "=r"(tm_frq));
+        tm_sec = tm_cnt/tm_frq;
+        print_i(tm_sec);
+        //print_s(".");
+		//print_i(tm_frq);
+        print_s("\n");
 	}else if(0 == strcmp(cmd, "reboot")){
-		;
+		reset(10);
 	}else if(0 == strcmp(cmd, "clear")){
 		print_s("\033[2J\033[1;1H");
 	}else{
@@ -99,8 +191,10 @@ void shell()
 
 int main(void)
 {
-    uart_puts("Bootloader running in QEMU GUI!\n");
-
+    uart1_init();
+    print_s("\033[2J\033[1;1H");
+    print_s("Bootloader running in QEMU GUI!\n");
+    
 	while(1) {
 		shell();
 	}
--- labs/qemu_launch.sh
+++ labs/qemu_launch.sh
@@ -1,5 +1,8 @@
-# 
-qemu-system-aarch64 -M raspi3 -m 1G -kernel kernel.img -serial stdio -display none
+# PL011-Uart
+#qemu-system-aarch64 -M raspi3b -m 1G -kernel kernel.img -serial stdio -display none
+
+# mini-Uart
+qemu-system-aarch64 -M raspi3b -m 1G -kernel kernel.img -serial null -serial stdio -display none
 
 # GDB
 #qemu-system-aarch64 -M raspi3 -m 1G -kernel kernel.img -serial stdio -display none -S -s
